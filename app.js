0a888815dafefd3c69e50a6bb3d475098a65523e
dejito <Deerealboy@gmail.com>
2025-01-13 19:21:35 +0100

    parsing incooming req

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-
-
 const express = require('express')
+const boodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/joor', (req, res, next)=> {
-    console.log("got to second midware");
-    res.send("<h2>Hello from the joor side</h2>");
+app.use(bodyParser.urlencoded({extended: false}));
+
+// app.use('/add-product', (req, res, next)=> {
+//     console.log("got to second middleware");
+//     res.send(
+//     <form action='/product' method='POST'><input type="text" name="title"></input><button type="submit">Add Product</button></form>);
+//     // res.send("<form><input type="text name="title"><button type="submit">Add Product<button/></form>');
+// });
+
+app.use('/add-product', (req, res, next) => {
+    console.log("got to second middleware");
+  
+    res.send(`
+      <form action='/product' method='POST'>
+        <input type="text" name="title">
+        <button type="submit">Add Product</button>
+      </form>
+    `); 
+  });
+
+app.use('/product', (req, res, next)=> {
+    console.log(req.body);
+    res.redirect('/');
+    next();
 });
+
 
 app.use('/', (req, res, next)=> {
     console.log("got to first midware");
@@ -19,4 +41,4 @@ app.use('/', (req, res, next)=> {
 
 // const server = http.createServer(app);
 
-app.listen(4001);
+app.listen(4002);
