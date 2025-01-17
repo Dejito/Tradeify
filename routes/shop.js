18b413f747e39189b821dd5d3516efb1f3f53b65
dejito <Deerealboy@gmail.com>
2025-01-17 17:54:23 +0100

    fixed bug --sendFile instead of ordinary send to sending back files

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next)=> {
     console.log("got to first midware");
-    res.send("<h2>Hello from the shop side</h2>");
-    next();
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
-module.exports = router;
+module.exports = router;    
