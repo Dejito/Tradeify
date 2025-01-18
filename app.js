0104c5ae514832bae89df2a88a52d8aa755b27a7
dejito <Deerealboy@gmail.com>
2025-01-18 15:55:56 +0100

    CSS styling implemented

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const express = require('express')
-const app = express();
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
-
+const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(shopRoutes);
 app.use('/admin', adminRoutes);
-// app.use('/', (res, req, next) => {
-//   if (res.)
-// });
+app.use(shopRoutes);
 
-app.use('/', (req, res, next)=>{
-      res.sendFile(path.join(__dirname, 'views', '404.html'));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-
-// const server = http.createServer(app);
-
-app.listen(4002);
+app.listen(3000);
