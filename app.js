0df60d74841b97815789ac57c609103614764566
dejito <Deerealboy@gmail.com>
2025-01-31 16:34:38 +0100

    fetch prods from db started

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+db.execute('SELECT * FROM products').then(prod => {
+    console.log('response is', prod);
+}).catch(error => {
+    console.log('error is .....', error);
+});
+
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -20,5 +28,6 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
 
 app.listen(3000);
