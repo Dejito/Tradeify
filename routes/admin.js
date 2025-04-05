ed7dcd435bd91bd2cd3578a50c9b94ca66577b76
dejito <Deerealboy@gmail.com>
2025-04-05 11:47:02 +0100

    shop page added

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next)=> {
-    // res.sendFile(path.join(__dirname, 'views', 'add-product.html'));
-
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
     // next();
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next)=> {
-    console.log("oga mario");
-    res.send('<h1>Welcome to dejito shop homepage </h1>');
+router.get('/', (req, res, next) => {
+    console.log('got to shop homepage');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    // res.send('<h1>Welcome to dejito shop homepage </h1>');
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+
 });
 
 module.exports = router;
