cd547b2dfd7d7af0346d0b510ae3cc017465d1fe
dejito <Deerealboy@gmail.com>
2025-04-05 12:03:16 +0100

    error page served

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -18,7 +20,8 @@ app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>page not found</h1>');
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).send('<h1>page not found</h1>');
 });
 
 app.listen(3000);
