100b1c6b6752650a7b726cbb033c68386d53a832
dejito <Deerealboy@gmail.com>
2026-02-28 16:52:42 +0100

    handling query param for edit product

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
+const path = require('path');
 
 const express = require('express');
 
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
