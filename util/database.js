d532fd3845963e825b4f007417eb054ff91fea0e
dejito <Deerealboy@gmail.com>
2026-03-15 21:59:12 +0100

    fixed db connect issue

--- util/database.js
+++ util/database.js
@@ -1,8 +1,39 @@
-const Sequelize = require('sequelize');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const sequelize = new Sequelize('tradeify', 'root', 'password', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const uri = 'mongodb+srv://dejito_db_user:Secret123@tradeify.emhzkl0.mongodb.net/tradeify?retryWrites=true&w=majority';
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = callback => {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  client
+    .connect()
+    .then(() => {
+      _db = client.db('tradeify');
+      console.log('Connected to MongoDB');
+      callback(_db);
+    })
+    .catch(err => {
+      console.error('MongoDB connect error:', err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw new Error('Database not initialized. Call mongoConnect first.');
+  }
+  return _db;
+};
+
+module.exports = {
+  mongoConnect,
+  getDb,
+};
